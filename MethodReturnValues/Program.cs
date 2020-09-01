1da7a2842606eb461d894896dc6a253414cb09c9
Angel Acosta <Mrnurdee@gmail.com>
2020-09-01 08:23:33 -0700

    Add project files.

---
+++ MethodReturnValues/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodReturnValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(15,12));
+            Console.WriteLine(Multiply(2,4));
+            Console.WriteLine(Subtract(30, 20));
+            Console.WriteLine(Divide(20,10));
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
